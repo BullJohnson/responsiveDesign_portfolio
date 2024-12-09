e96dfb678c60c4a063156576f355eb759790b950
bulljohnson <bulljohnson44@gmail.com>
2024-12-08 23:42:37 -0500

    Finakl Project for CS 240, Responsive design

---
+++ script.js
@@ -0,0 +1,11 @@
+
+"use strict";
+
+            // Portfolio - Responsive Web Design, CS240 | William S. Johnson, Jr
+
+
+$(document).ready( () => {
+
+    // runs when an h2 heading is clicked
+
+});
